002c02b7a65afbaa5d0309726862bbf38aec5c2f
Nick Spreitzer <nick@refactorsaurusrex.com>
2017-02-21 23:06:54 -0600

    Allow DND function to be disabled

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ChromeDND
+
+## Known Issues
+- The browser icon fails to update when turning DND on/off in quick succession. As long as you wait 2 - 3 seconds to enable DND after disabling it, or vise versa, the icon will update correctly.
--- src/eventPage.js
+++ src/eventPage.js
@@ -1,7 +1,13 @@
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name !== 'chrome-dnd-snooze') return;
+
     chrome.contentSettings['notifications'].set({
       'primaryPattern': '<all_urls>',
       'setting': 'allow'
     });
+
+    chrome.browserAction.setIcon({path: 'images\\bell-gray-16.png'});
+    chrome.browserAction.setPopup({popup: 'dnd-inactive.html'});
+
+    // Display notification here
 });
